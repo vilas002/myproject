4c7fbbad91a3347f3fc720c676c8c18c6bd9e670
vilas002 <pattepuvilas@gmail.com>
2026-07-17 21:51:42 +0530

    removed one line

--- basic.java
+++ basic.java
@@ -1,6 +1,6 @@
 class Main{
 	public static void main(String[] args){
 	System.out.println("Wellcome to Java Learning Course!");
-	System.out.println("Good Morning");
+
 	}
 }
---
+++ readingInput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Basic{
+	public static void main(String[] args){
+	System.out.println("Enter Name");
+	Scanner sc = new Scanner(System.in);
+	String name = sc.nextLine();
+	System.out.println(name);
+	sc.close();
+
+	}
+}
